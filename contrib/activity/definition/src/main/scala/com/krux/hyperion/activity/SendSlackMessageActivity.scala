729c141036b38cc55d02a630252da78d0b8d09db
Seth Yates <seth.yates@krux.com>
2016-02-13 00:17:37 -0800

    (Fixes #337) Implement SendSlackMessage

---
+++ contrib/activity/definition/src/main/scala/com/krux/hyperion/activity/SendSlackMessageActivity.scala
@@ -0,0 +1,64 @@
+package com.krux.hyperion.activity
+
+import com.krux.hyperion.HyperionContext
+import com.krux.hyperion.adt.{ HBoolean, HString }
+import com.krux.hyperion.common.{ BaseFields, PipelineObjectId, S3Uri }
+import com.krux.hyperion.expression.RunnableObject
+import com.krux.hyperion.resource.{ Ec2Resource, Resource }
+
+case class SendSlackMessageActivity private (
+  baseFields: BaseFields,
+  activityFields: ActivityFields[Ec2Resource],
+  shellCommandActivityFields: ShellCommandActivityFields,
+  jarUri: HString,
+  mainClass: HString,
+  continueOnError: HBoolean,
+  webhookUrl: HString,
+  message: Seq[HString],
+  user: Option[HString],
+  emoji: Option[HString],
+  to: Option[HString]
+) extends BaseShellCommandActivity {
+
+  type Self = SendSlackMessageActivity
+
+  def updateBaseFields(fields: BaseFields) = copy(baseFields = fields)
+  def updateActivityFields(fields: ActivityFields[Ec2Resource]) = copy(activityFields = fields)
+  def updateShellCommandActivityFields(fields: ShellCommandActivityFields) = copy(shellCommandActivityFields = fields)
+
+  def continuingOnError = copy(continueOnError = true)
+  def withUser(user: HString) = copy(user = Option(user))
+  def withEmoji(emoji: HString) = copy(emoji = Option(emoji))
+  def toUser(user: HString) = copy(to = Option(s"@$user": HString))
+  def toChannel(channel: HString) = copy(to = Option(s"#$channel": HString))
+
+  private def arguments: Seq[HString] = Seq(
+    if (continueOnError) None else Option(Seq[HString]("--fail-on-error")),
+    Option(Seq[HString]("--webhook-url", webhookUrl)),
+    user.map(user => Seq[HString]("--user", user)),
+    emoji.map(emoji => Seq[HString]("--emoji", emoji)),
+    to.map(to => Seq[HString]("--to", to))
+  ).flatten.flatten ++ message
+
+  override def scriptArguments = (jarUri.serialize: HString) +: mainClass +: arguments
+
+}
+
+object SendSlackMessageActivity extends RunnableObject {
+
+  def apply(webhookUrl: HString, message: HString*)(runsOn: Resource[Ec2Resource])(implicit hc: HyperionContext): SendSlackMessageActivity =
+    new SendSlackMessageActivity(
+      baseFields = BaseFields(PipelineObjectId(SendSlackMessageActivity.getClass)),
+      activityFields = ActivityFields(runsOn),
+      shellCommandActivityFields = ShellCommandActivityFields(S3Uri(s"${hc.scriptUri}activities/run-jar.sh")),
+      jarUri = s"${hc.scriptUri}activities/hyperion-notification-activity-current-assembly.jar",
+      mainClass = "com.krux.hyperion.contrib.activity.notification.SendSlackMessage",
+      continueOnError = false,
+      webhookUrl = webhookUrl,
+      message = message,
+      user = None,
+      emoji = None,
+      to = None
+    )
+
+}
---
+++ contrib/activity/notification/src/main/scala/com/krux/hyperion/contrib/activity/notification/SendSlackMessage.scala
@@ -0,0 +1,77 @@
+package com.krux.hyperion.contrib.activity.notification
+
+import java.net.{ HttpURLConnection, URL }
+
+import org.json4s.JsonAST.{ JString, JObject }
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+import scopt.OptionParser
+
+object SendSlackMessage {
+  case class Options(
+    failOnError: Boolean = false,
+    webhookUrl: String = "",
+    user: Option[String] = None,
+    message: Seq[String] = Seq.empty,
+    iconEmoji: Option[String] = None,
+    channel: Option[String] = None
+  )
+
+  def apply(options: Options): Boolean = try {
+    // Setup the connection
+    val connection = new URL(options.webhookUrl).openConnection().asInstanceOf[HttpURLConnection]
+    connection.setDoOutput(true)
+    connection.setRequestProperty("Content-Type", "application/json")
+    connection.setRequestProperty("Accept", "application/json")
+
+    // Write the message
+    val output = connection.getOutputStream
+    try {
+      val message = Seq(
+        "icon_emoji" -> options.iconEmoji,
+        "channel" -> options.channel,
+        "username" -> options.user,
+        "text" -> Option(options.message.mkString("\n"))
+      ).flatMap {
+        case (k, None) => None
+        case (k, Some(v)) => Option(k -> JString(v))
+      }
+
+      output.write(compact(render(JObject(message: _*))).getBytes)
+    } finally {
+      output.close()
+    }
+
+    // Check the response code
+    connection.getResponseCode == 200 || !options.failOnError
+  } catch {
+    case e: Throwable =>
+      System.err.println(e.toString)
+      !options.failOnError
+  }
+
+  def main(args: Array[String]): Unit = {
+    val parser = new OptionParser[Options](s"hyperion-notification-flowdock-activity") {
+      override def showUsageOnError = true
+
+      note("Sends a notification message to a Slack incoming webhook.")
+      help("help").text("prints this usage text")
+      opt[Unit]("fail-on-error").optional().action((_, c) => c.copy(failOnError = true))
+        .text("Causes the activity to fail if any error received from the webhook")
+      opt[String]("webhook-url").valueName("WEBHOOK").required().action((x, c) => c.copy(webhookUrl = x))
+        .text("Sends the message to the given WEBHOOK url")
+      opt[String]("user").valueName("NAME").optional().action((x, c) => c.copy(user = Option(x)))
+        .text("Sends the message as the user with NAME")
+      opt[String]("emoji").valueName("EMOJI").optional().action((x, c) => c.copy(iconEmoji = Option(x)))
+        .text("Use EMOJI for the icon")
+      opt[String]("to").valueName("CHANNEL or USERNAME").optional().action((x, c) => c.copy(channel = Option(x)))
+        .text("Sends the message to #CHANNEL or @USERNAME")
+      arg[String]("MESSAGE").required().unbounded().action((x, c) => c.copy(message = c.message :+ x))
+        .text("Sends the given MESSAGE")
+    }
+
+    if (!parser.parse(args, Options()).exists(apply)) {
+      System.exit(3)
+    }
+  }
+}
