ffed27ff47d92ffa1fe8d90a95d269b073822ae6
Kexin Xie <ikexinxie@gmail.com>
2016-06-30 14:54:46 -0700

    (Fixes #423) Add the exponential backoff and jitter to retry (#424)

--- core/src/main/scala/com/krux/hyperion/client/Retry.scala
+++ core/src/main/scala/com/krux/hyperion/client/Retry.scala
@@ -1,7 +1,10 @@
 package com.krux.hyperion.client
 
+import scala.util.Random
+
 import com.amazonaws.AmazonServiceException
 import org.slf4j.Logger
+
 
 /**
  * Performs retry on throttle
@@ -12,21 +15,20 @@ trait Retry {
 
   def maxRetry: Int
 
-  def retryDelay = 5000  // in miliseconds
-
   implicit class Retryable[A](action: => A) {
 
-    def retry(n: Int = maxRetry): A = {
-      if (n > 0)
+    def retry(n: Int = 0): A = {
+      if (n < maxRetry)
         try {
           action
         } catch {
           // use startsWith becase the doc says the error code is called "Throttling" but sometimes
           // we see "ThrottlingException" instead
           case e: AmazonServiceException if e.getErrorCode().startsWith("Throttling") && e.getStatusCode == 400 =>
-            log.warn(s"caught exception: ${e.getMessage}\n Retry after 5 seconds...")
-            Thread.sleep(retryDelay)
-            retry(n - 1)
+            val retryDelay = Random.nextInt(Math.pow(2, (n + 1)).toInt) + 5
+            log.warn(s"caught exception: ${e.getMessage}\n Retry (No. $n) after $retryDelay seconds...")
+            Thread.sleep(retryDelay * 1000)
+            retry(n + 1)
         }
       else
         action
