880498d4f5f19677375e09f96b7e34be453e4071
Pulkit Khetrapal <pkhetrapal@krux.com>
2016-10-21 12:53:55 -0700

    (Closes #471) Adds aws cp with profile activity

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## 4.5.2 - 2016-10-20
+### Added
+- [#471](https://github.com/krux/hyperion/pull/471) - Add support for `AWS S3 cp` CLI arguments in AwsS3CpActivity
+
 ## 4.5.1 - 2016-10-19
 - [#467](https://github.com/krux/hyperion/issues/467) - Handle InvalidRequestException properly during pipeline creation
 
---
+++ contrib/activity/definition/src/main/scala/com/krux/hyperion/activity/AwsS3CpActivity.scala
@@ -0,0 +1,69 @@
+package com.krux.hyperion.activity
+
+import com.krux.hyperion.adt.{HBoolean, HS3Uri, HString}
+import com.krux.hyperion.common.{BaseFields, PipelineObjectId}
+import com.krux.hyperion.resource.{Ec2Resource, Resource}
+import com.krux.hyperion.expression.RunnableObject
+
+case class AwsS3CpActivity private(
+  baseFields: BaseFields,
+  activityFields: ActivityFields[Ec2Resource],
+  shellCommandActivityFields: ShellCommandActivityFields,
+  sourceS3Path: HS3Uri,
+  destinationS3Path: HS3Uri,
+  isRecursive: HBoolean,
+  isOverwrite: HBoolean,
+  additionalArguments: Seq[HString]
+) extends BaseShellCommandActivity with WithS3Input {
+
+  type Self = AwsS3CpActivity
+
+  def updateBaseFields(fields: BaseFields) = copy(baseFields = fields)
+  def updateActivityFields(fields: ActivityFields[Ec2Resource]) = copy(activityFields = fields)
+  def updateShellCommandActivityFields(fields: ShellCommandActivityFields) = copy(shellCommandActivityFields = fields)
+
+  def withOverwrite() = copy(isOverwrite = HBoolean.True)
+  def withRecursive() = withAdditionalArguments("--recursive")
+  def withProfile(profile: HString) = withAdditionalArguments("--profile", profile)
+  def withAcl(cannedAcl: HString) = withAdditionalArguments("--acl", cannedAcl)
+  def withExclude(pattern: HString) = withAdditionalArguments("--exclude", pattern)
+  def withInclude(pattern: HString) = withAdditionalArguments("--include", pattern)
+  def withSourceRegion(sourceRegion: HString) = withAdditionalArguments("--source-region", sourceRegion)
+  def withDestinationRegion(destinationRegion: HString) = withAdditionalArguments("--region", destinationRegion)
+  def withGrant(permission: HString, granteeTypeAndId: Seq[(String, String)]) = withAdditionalArguments("--grants",
+    granteeTypeAndId.map { case (grantType, id) => s"$grantType=$id" }.mkString(s"$permission=", ",", "")
+  )
+  def withAdditionalArguments(arguments: HString*) = copy(additionalArguments = this.additionalArguments ++ arguments)
+
+  private val removeScript = if (isOverwrite) s"aws s3 rm --recursive $destinationS3Path;" else ""
+
+  private val s3CpScript = s"aws s3 cp ${additionalArguments.mkString(" ")} $sourceS3Path $destinationS3Path;"
+
+  override def script = s"""
+    |if [ -n "$${INPUT1_STAGING_DIR}" ]; then
+    | mkdir -p ~/.aws
+    | cat $${INPUT1_STAGING_DIR}/credentials >> ~/.aws/credentials;
+    | cat $${INPUT1_STAGING_DIR}/config >> ~/.aws/config;
+    |fi
+    |$removeScript
+    |$s3CpScript
+  """.stripMargin
+}
+
+object AwsS3CpActivity extends RunnableObject {
+
+  def apply(
+    sourceS3Path: HS3Uri,
+    destinationS3Path: HS3Uri
+  )(runsOn: Resource[Ec2Resource]): AwsS3CpActivity =
+    new AwsS3CpActivity(
+      baseFields = BaseFields(PipelineObjectId(AwsS3CpActivity.getClass)),
+      activityFields = ActivityFields(runsOn),
+      shellCommandActivityFields = ShellCommandActivityFields(s"aws s3 cp $sourceS3Path $destinationS3Path"),
+      sourceS3Path = sourceS3Path,
+      destinationS3Path = destinationS3Path,
+      isRecursive = HBoolean.False,
+      isOverwrite = HBoolean.False,
+      additionalArguments = Seq.empty
+    )
+}
